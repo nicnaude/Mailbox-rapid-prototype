5350a7402880cb896e2d6fc1ce24432f47c84228
Nicholas Naudé <nicnaude@gmail.com>
2016-10-29 12:25:10 -0700

    Initial Commit

---
+++ Mailbox/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Mailbox
+//
+//  Created by Nicholas Naudé on 29/10/2016.
+//  Copyright © 2016 Nicholas Naudé. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
