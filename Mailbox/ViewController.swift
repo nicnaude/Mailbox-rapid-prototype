5861df4f8fba6d36c037767ac8d8966fc0ef4228
Nicholas Naudé <nicnaude@gmail.com>
2016-10-29 20:58:59 -0700

    Implements all views

--- Mailbox/ViewController.swift
+++ Mailbox/ViewController.swift
@@ -10,16 +10,36 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // Outlets
+    @IBOutlet weak var menuImageView: UIImageView!
+    @IBOutlet weak var feedImageView: UIImageView!
+    @IBOutlet weak var rescheduleImageView: UIImageView!
+    @IBOutlet weak var messageImageView: UIImageView!
+    @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var feedPanelView: UIView!
+    
+    // Variables
+    
+    
+    //Lifecyle methods
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        // Hide items
+        menuImageView.isHidden = true
+        rescheduleImageView.isHidden = true
+        
+        scrollView.contentSize = CGSize(width: 320, height: 2200)
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    // Functions
+    @IBAction func onPanButtonDrag(_ sender: UIPanGestureRecognizer) {
+    print("drag")
+        menuImageView.isHidden = false
     }
-
-
-}
+    
+    @IBAction func onMessagePanDrag(_ sender: UIPanGestureRecognizer) {
+    print("Message drag")
+    }
+    
+} // The end
 
